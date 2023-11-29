f71be8411bed6be077a27e2f469fb2b223bf950a
mrgarshin <garshin.serg@yandex.ru>
2023-11-29 15:42:18 +0300

    feat: add sumOfCodes task solving

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -179,8 +179,8 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str = '') {
+  return str ? [...str].reduce((acc, item) => acc + item.charCodeAt(), 0) : 0;
 }
 
 /**
